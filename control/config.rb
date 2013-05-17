2f838c6e6066449c364e7d7e2ba22915b00262fc
yanzhai <zhaiyan920@gmail.com>
2013-05-17 14:12:00 -0500

    remove sec key for aws

--- control/config.rb
+++ control/config.rb
@@ -4,6 +4,6 @@ COMMON_ARGS = {
   :recever_port => 1985,
   :sendsize => 100 * 1024 * 1024,
   :ami_id => "ami-41157c28",
-  :aws_access_key => "AKIAIURU4SIO3ITY7CIA",
-  :aws_secret_key => "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"
+  :aws_access_key => "",
+  :aws_secret_key => ""
 }
