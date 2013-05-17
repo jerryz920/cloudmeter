baad223b7402a1a9df002aab542715465df54234
yanzhai <zhaiyan920@gmail.com>
2013-05-17 13:59:48 -0500

    changed the ami id

--- control/config.rb
+++ control/config.rb
@@ -3,7 +3,7 @@ COMMON_ARGS = {
   :localhost => "lemma920.cs.wisc.edu",
   :recever_port => 1985,
   :sendsize => 100 * 1024 * 1024,
-  :ami_id => "ami-e5026b8c",
+  :ami_id => "ami-41157c28",
   :aws_access_key => "AKIAIURU4SIO3ITY7CIA",
   :aws_secret_key => "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"
 }
